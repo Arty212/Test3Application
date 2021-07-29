65e81dd01ddfa35d0d7444d013b8028fdc90550c
Arty212 <Arty212@mail.ru>
2021-07-29 13:39:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/test3application/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.test3application
+
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showNotification(applicationContext, "123", "123")
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun showNotification(
+        context: Context,
+        title: String?,
+        message: String?
+    ) {
+        val ii = Intent(context, MainActivity::class.java).also {
+            it.data = Uri.parse("custom://" + System.currentTimeMillis())
+            it.action = "actionstring" + System.currentTimeMillis()
+            it.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pi =
+            PendingIntent.getActivity(context, 0, ii, PendingIntent.FLAG_UPDATE_CURRENT)
+        val notification: Notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pi)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setWhen(System.currentTimeMillis())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentTitle(title).build()
+            val notificationManager = context.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                title,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        } else {
+            notification = NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message)
+                .setContentIntent(pi)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentTitle(title).build()
+            val notificationManager = context.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+    }
+}
